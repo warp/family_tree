92b9c07a038eccc0fdb52c36869431c1e25c412c
Sarah Hay <sarah.hay@trineo.com>
2020-10-05 20:15:04 +1300

    Expand 404 endpoint

--- lib/endpoint/four_oh_four.rb
+++ lib/endpoint/four_oh_four.rb
@@ -1,7 +1,15 @@
+require "rack/request"
+require "rack/response"
+
 module Endpoint
   class FourOhFour
     def self.call(env)
-      return ["text/plain", 404, 'not found']
+      request = Rack::Request.new(env)
+      response = Rack::Response.new
+      response.headers["Content-Type"] = "text/plain"
+      response.status = 404
+      response.body = ['Not Found']
+      return response.finish
     end
   end
 end
